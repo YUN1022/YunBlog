7c33f61ff4186937eba20fc237e06dc2988142d4
yun <a3302a0229@gmail.com>
2024-06-02 15:09:32 +0800

    feat(main): 調整連結

--- docs/.vitepress/theme/components/Feature.vue
+++ docs/.vitepress/theme/components/Feature.vue
@@ -1,18 +1,13 @@
 <script setup lang="ts">
-import { data as posts } from '../utlis/post.data.mjs'
-import type {Post} from '../utlis/post.data.mjs'
-
-const link = (post: Post) => {
-  location.href=post.url
-}
-
+import {data as posts} from '../utlis/post.data.mjs'
+import {withBase} from "vitepress";
 </script>
 
 <template>
   <div class="theme-container">
     <ul>
       <li class="w-full block rounded-2xl px-8 mb-3 border-2 border-[#767764] cursor-pointer" v-for="post of posts">
-        <a class="flex flex-col" @click="link(post)">
+        <a class="flex flex-col" :href="withBase(post.url)">
           <h1>{{ post.title }}</h1>
           <span>{{post.summary}}</span>
           <span>{{post.date}}</span>
