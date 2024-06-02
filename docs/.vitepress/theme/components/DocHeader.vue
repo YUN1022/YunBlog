da020569734780c99ff3c4a729bf66582b66874a
yun <a3302a0229@gmail.com>
2024-06-02 16:37:23 +0800

    feat(main): 新增文章 header

---
+++ docs/.vitepress/theme/components/DocHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex flex-col items-center">
+    <h1 class="mb-7 font-bold text-[2.5em]">{{ post.title }}</h1>
+    <span>📅 {{ post.date }}</span>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+import {useData} from "vitepress";
+import {formatPost} from "../utlis/utlis";
+
+const pageData = useData()
+const post = formatPost(pageData.page.value)
+
+</script>
+
+<style scoped>
+
+</style>
--- docs/.vitepress/theme/components/ThemeLayout.vue
+++ docs/.vitepress/theme/components/ThemeLayout.vue
@@ -1,18 +1,27 @@
-<script setup>
+<template>
+  <Layout>
+    <template #home-hero-before>
+      <Home/>
+    </template>
+    <template #home-features-before>
+      <Feature/>
+    </template>
+    <template #doc-before>
+      <DocHeader/>
+    </template>
+    <template #doc-after></template>
+  </Layout>
+</template>
+
+<script lang="ts" setup>
 import DefaultTheme from "vitepress/theme";
 import Home from "./Home.vue";
 import Feature from "./Feature.vue";
+import DocHeader from "./DocHeader.vue";
 
 const Layout = DefaultTheme.Layout
 
 </script>
-
-<template>
-  <Layout>
-    <template #home-hero-before> <Home/> </template>
-    <template #home-features-before> <Feature /></template>
-  </Layout>
-</template>
 
 
 <style scoped>
