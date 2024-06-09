bf2d46f59e8ec5131d861db297460e51e9b566a4
yun <a3302a0229@gmail.com>
2024-06-09 23:56:27 +0800

    feat(main): 調整文章標題 css

--- docs/.vitepress/theme/components/Feature.vue
+++ docs/.vitepress/theme/components/Feature.vue
@@ -6,11 +6,11 @@ import {withBase} from "vitepress";
 <template>
   <div class="theme-container">
     <ul>
-      <li class="w-full block rounded-2xl px-8 mb-3 border-2 border-[#767764] cursor-pointer" v-for="post of posts">
+      <li class="w-full block rounded-2xl px-8 mb-3 border-2 border-[#767764]" v-for="post of posts">
         <a class="flex flex-col" :href="withBase(post.url)">
-          <h1>{{ post.title }}</h1>
-          <span>{{post.summary}}</span>
-          <span>{{post.date}}</span>
+          <h1 class="mb-3 text-xl font-medium">{{ post.title }}</h1>
+          <span class = "mb-2 opacity-80 font-light">{{post.summary}}</span>
+          <span class="text-right">{{post.date}}</span>
         </a>
       </li>
     </ul>
